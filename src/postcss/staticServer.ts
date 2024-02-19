d4dc474532e42b6cb6f96dd62fbf2f0eb358eb64
mimiMonads <mimi@monads.dev>
2024-02-19 22:43:15 +0000

    postcss pre maps

---
+++ src/postcss/staticServer.ts
@@ -0,0 +1,43 @@
+import postcss from 'postcss'
+import fs from 'node:fs'
+
+type uses = postcss.AcceptedPlugin[]
+
+
+type StaticServer = {
+  uses?: uses;
+};
+
+
+  export const postcssStaticServer =
+  (post : typeof postcss) => (option?: StaticServer) =>
+    (
+      (pro) => ({
+        checker: (path: string) => path.includes(".css"),
+        r: (ob: {
+          root: string;
+          path: string;
+          relativeName: string;
+        }) => ({
+          type: "response",
+          path: ob.relativeName,
+          r: ((v) => async () =>
+            new Response(
+              v === ""
+                ? 
+                  v = await pro
+                    .process(fs.readFileSync(ob.path, "utf8") , { from: ob.path, })
+                    .then(result => result.css)
+            
+                : v,
+              {
+                headers: new Headers([
+                  ["content-type", "text/css"],
+                ]),
+              },
+            ))(""),
+        } as const),
+      })
+    )(
+      post(option?.uses ?? [])
+    );
---
+++ test/postcss/serve.deno.test.ts
@@ -0,0 +1,33 @@
+import { vixeny } from "vixeny";
+import postcssNested from 'postcss-nested'
+import autoprefixer from 'autoprefixer'
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { postcssStaticServer } from "../../src/postcss/staticServer.ts";
+import postcss from "postcss";
+
+const serve = vixeny()([
+  {
+    type: "fileServer",
+    name: "/",
+    path: "./public/postcss",
+    template: [
+        postcssStaticServer(postcss)(
+        {
+          uses: [
+            autoprefixer , postcssNested 
+          ] as (postcss.AcceptedPlugin)[] ,
+        },
+      ),
+    ],
+  },
+]);
+const normalize = (s: string) =>
+  s.replace(/(?<!`|\$\{.*)(["'])(?:(?=(\\?))\2.)*?\1/g, " ")
+    .replace(/\s+/g, " ")
+    .replace(/ +/g, " ");
+
+Deno.test("compile", async () => {
+  const response = await serve(new Request("http://localhost:8080/main.css"));
+  const text = normalize(await response.text());
+  assertEquals(text,normalize('/* Input CSS (styles.css) */ :root { --main-color: #123456; } .body { font-family: , sans-serif; color: var(--main-color); } .body .header { background: var(--main-color); } .body .header__title { margin: 0; color: #ffffff; } '));
+});
